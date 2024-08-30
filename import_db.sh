2ad8d6e4986dac7a4283794548415a91ce29f881
Adam Patterson <adam@paper-leaf.com>
2024-08-30 11:41:58 -0600

    Adds GZ support

--- import_db.sh
+++ import_db.sh
@@ -8,17 +8,26 @@ MYSQL_PASSWORD="root"
 # Set the directory containing SQL files
 SQL_DIR="./backups"
 
-# Iterate over SQL files in the directory
-for sql_file in "$SQL_DIR"/*.sql; do
+# Iterate over SQL and GZ files in the directory
+for file in "$SQL_DIR"/*.{sql,gz}; do
+    # Check if file exists (to handle case when no .sql or .gz files are found)
+    [ -e "$file" ] || continue
+
     # Extract filename without extension
-    filename=$(basename "$sql_file")
+    filename=$(basename "$file")
     db_name="${filename%.*}"
 
-    # Import SQL file into MySQL
-    echo "Importing $sql_file into database $db_name..."
+    # Import SQL file or GZ file into MySQL
+    echo "Importing $file into database $db_name..."
     mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$db_name\`;"
-    mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$db_name" < "$sql_file"
-    echo "Import completed for $sql_file"
+
+    if [[ "$file" == *.gz ]]; then
+        gunzip < "$file" | mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$db_name"
+    else
+        mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$db_name" < "$file"
+    fi
+
+    echo "Import completed for $file"
 done
 
-echo "All SQL files imported successfully."
+echo "All SQL and GZ files imported successfully."
