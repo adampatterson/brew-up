edd030573c7d5ca3793f5a8a811bf8c34868777a
Adam Patterson <adam@paper-leaf.com>
2024-10-16 10:27:15 -0600

    Cleanup!

--- setup_dev.sh
+++ setup_dev.sh
@@ -2,9 +2,7 @@
 brew tap shivammathur/extensions
 
 # PHP Versions
-brew install php@8.2
-brew install php@8.3
-brew install php@8.4
+brew install php@8.2 php@8.3 php@8.4
 
 # PHP Extensions
 brew install xdebug2@8.2 igbinary@8.2 msgpack@8.2 memcached@8.2 redis@8.2 imagick@8.2 memcached@8.2 redis@8.2
@@ -27,3 +25,6 @@ brew install nvm
 # Nice to have
 brew install tree
 brew install logrotate
+brew install htop
+brew install cloudflared
+brew install warp
