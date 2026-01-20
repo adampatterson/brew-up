290c7105d1a8b57d112bd57683c72e12c7fd0167
Adam Patterson <adam@paper-leaf.com>
2026-01-20 13:28:52 -0700

    Updated brew script

--- setup_dev.sh
+++ setup_dev.sh
@@ -5,13 +5,17 @@ brew tap shivammathur/extensions
 brew install mysql@8.4
 brew install memcached
 brew install redis
+#brew install postgresql
+#brew install neo4j
+#brew install --cask clickhouse
+#brew install weaviate
+#brew install weaviate-cli
 
 # PHP Versions
-brew install php@8.2 php@8.3 php@8.4
+brew install php@8.2 php@8.4
 
 # PHP Extensions
 brew install xdebug2@8.2 igbinary@8.2 msgpack@8.2 memcached@8.2 redis@8.2 imagick@8.2
-brew install xdebug@8.3 igbinary@8.3 msgpack@8.3 memcached@8.3 redis@8.3 imagick@8.3
 brew install xdebug@8.4 igbinary@8.4 msgpack@8.4 memcached@8.4 redis@8.4 imagick@8.4
 
 # Nginx
@@ -23,11 +27,15 @@ brew install node
 brew install nvm
 
 # Nice to have
+brew install rclone
+brew install awscli
 brew install tree
+brew install gpg
 brew install logrotate
 brew install htop
 brew install cloudflared
 brew install warp
+brew install act
 brew install wp-cli
 brew install --cask raycast
 brew install --cask rectangle
